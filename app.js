61209d5ed207d71a35c71cd5b72d0a73aba58465
Simon <cells800@gmail.com>
2017-05-17 04:39:23 +0000

    User setup done

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var expressHbs = require('express-handlebars');
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+var session = require('express-session');
 
-var index = require('./routes/index');
+var routes = require('./routes/index');
 
 var app = express();
 
@@ -23,9 +24,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({secret: 'mysupersecret', resave: false, saveUninitialized: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
